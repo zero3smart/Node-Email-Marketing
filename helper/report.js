412c89096d1b3f90f902f2f7a0773fd1565a3a5c
Titu <email.to.titu@gmail.com>
2016-11-17 00:37:18 +0600

    pdf reporting done

--- helper/report.js
+++ helper/report.js
@@ -7,22 +7,23 @@ const settings = require('../config/settings');
 const commonHelper = require('./common');
 const promise = require('bluebird');
 const zipHelper = require('./zip');
+const pdf = require('html-pdf');
 
-let saveReports = (results, directory, header) => {
+let saveReports = (report, directory, header) => {
 
     let cleanDirectory = directory + '/' + settings.cleanDirectory + '/';
 
-    return promise.map(results, function (result) {
-        if(!result.report.saveReports) {
+    return promise.map(report.files, function (fileReport) {
+        if (!fileReport.reports) {
             return;
         }
         else {
-            let fileName = result.report.fileName;
+            let fileName = fileReport.fileName;
             let fileExtension = commonHelper.getFileExtension(fileName).toLowerCase();
             let fileNameWithoutExtension = fileName.split('.')[0];
             let handler = commonHelper.geFileHandler(fileExtension);
             let delimiter = null;
-            return promise.map(result.report.saveReports, function (reportToSave) {
+            return promise.map(fileReport.reports, function (reportToSave) {
 
                 var data = _.map(reportToSave.data, function (d) {
                     return [d];
@@ -32,7 +33,7 @@ let saveReports = (results, directory, header) => {
             })
                 .then(() => {
                     //write the clean file
-                    return handler.save(result.data, cleanDirectory, ('CLEANED_' + fileNameWithoutExtension), header, delimiter);
+                    return handler.save(fileReport.data, cleanDirectory, ('CLEANED_' + fileNameWithoutExtension), header, delimiter);
                 });
         }
     })
@@ -40,10 +41,102 @@ let saveReports = (results, directory, header) => {
             return zipHelper.zip(cleanDirectory, new Date().getTime().toString() + '.zip', 'zip');
         })
         .then(() => {
-            return results;
+            return createPDFReport(report, directory);
         });
 
 };
+
+let createPDFReport = (report, directory) => {
+
+    let cleanDirectory = directory + '/' + settings.cleanDirectory + '/';
+
+    let options = {
+        "format": "Letter",
+        /*"header": {
+         "height": "1.5in",
+         "contents": '<div style="text-align: center;">Email Scrubbing Report</div>'
+         },*/
+        "border": {
+            "top": "0.5in",            // default is 0, units: mm, cm, in, px
+            "right": "0.5in",
+            "bottom": "0.5in",
+            "left": "0.5in"
+        }
+    };
+    let tableString = [
+        '<table cellpadding="2" style="border: 0px">',
+        keyValueRow('Customer:', report.userName),
+        keyValueRow('File:', report.fileId),
+        keyValueRow('Date:', new Date()),
+        keyValueRow('Total pre clean emails:', report.totalPreCleanRecords),
+        keyValueRow('Total cleaned emails:', report.totalRecordsAfterClean),
+        keyValueRow('Time required to clean:', report.timeRequired),
+        '</table>'
+    ].join('');
+
+
+    report.files.forEach(function (file) {
+        tableString += [
+            '<table cellpadding="5" class="border-table">',
+            keyValueRow('File Name', file.fileName),
+            keyValueRow('Pre clean emails', file.totalRecords),
+            keyValueRow('Cleaned emails', file.data.length),
+        ].join('');
+        file.reports.forEach(function (fileReport) {
+            tableString += keyValueRow((fileReport.reportName), fileReport.data.length)
+        });
+
+        tableString += '</table>'
+    });
+
+    let html = '<div class="title">Email Scrubbing Report</div>' + pdfHeaderTemplate + css + tableString + pdfFooterTemplate;
+
+    return new promise(function (resolve, reject) {
+        pdf.create(html, options).toFile(cleanDirectory + 'report.pdf', function (err, res) {
+            if (err) {
+                console.log(err);
+                reject(err);
+            }
+            resolve();
+        });
+    });
+
+};
+
+let keyValueRow = (key, value) => {
+    return [
+        '<tr>',
+        '<td>' + key + '</td>',
+        '<td>' + value + '</td>',
+        '</tr>'
+    ].join('');
+};
+
+let pdfHeaderTemplate = [
+    '<!DOCTYPE html>',
+    '<html lang="en">',
+    '<head>',
+    '<meta charset="UTF-8">',
+    '<title>Title</title>',
+    '</head>',
+    '<body>'
+].join('');
+
+let css = [
+    '<style>',
+    '.title {text-align: center; margin-bottom: 50px;font-size: 14px; font-weight: bold;}',
+    'body {font-family:Verdana,Georgia,Serif; }',
+    'table {width: 100%;font-size:12px; margin: 15px 0px; }',
+    'table.border-table { border-collapse:collapse; border:1px solid #000000;}',
+    'table.border-table td { border:1px solid #000000; }',
+    'thead { font-weight: bold; }',
+    '</style>'
+].join('');
+
+let pdfFooterTemplate = [
+    '</body>',
+    '</html>',
+].join('');
 
 module.exports = {
     saveReports: saveReports
